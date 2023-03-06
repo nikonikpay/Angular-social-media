8f719691b615c4bfdb5adb1d7ce97ed78ba962a4
nikonikpay <nikonikpay@gmail.com>
2023-03-06 19:37:35 +0100

    the first part of the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {CreateAccountComponent} from "src/app/pages/create-account/create-account.component";
+import {LoginComponent} from "src/app/pages/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'create-account', component: CreateAccountComponent}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+            imports: [RouterModule.forRoot(routes)],
+            exports: [RouterModule]
+          })
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
+import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {TopbarComponent} from './components/topbar/topbar.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {LoginComponent} from './pages/login/login.component';
+import { CreateAccountComponent } from './pages/create-account/create-account.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+            declarations: [
+              AppComponent,
+              TopbarComponent,
+              LoginComponent,
+              CreateAccountComponent
+            ],
+            imports     : [
+              BrowserModule,
+              AppRoutingModule,
+              BrowserAnimationsModule,
+              MatButtonModule,
+              MatFormFieldModule,
+              MatInputModule,
+              MatSnackBarModule,
+              ReactiveFormsModule,
+              HttpClientModule
+            ],
+            providers   : [],
+            bootstrap   : [AppComponent]
+          })
+export class AppModule {
+}
